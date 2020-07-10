2b421f2c2a0cb7e900e1653839b5875621cf5bbf
Selawen <37039183+Selawen@users.noreply.github.com>
2020-07-10 23:48:47 +0200

    movement
    movement works!

--- Assets/Scripts/ChangeKey.cs
+++ Assets/Scripts/ChangeKey.cs
@@ -10,15 +10,32 @@ public class ChangeKey : MonoBehaviour
     public KeyCode left { get; private set; }
     public KeyCode jump { get; private set; }
 
+    KeyCode[] usableKeycodes;
     // Start is called before the first frame update
     void Start()
     {
-        
+        usableKeycodes = new KeyCode[] { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.T, , KeyCode.Y, KeyCode.U, KeyCode.I, KeyCode.O, KeyCode.P, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.V, KeyCode.B, KeyCode.N, KeyCode.M, KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow };
+        up = KeyCode.W;
+        down = KeyCode.S;
+        right = KeyCode.D;
+        left = KeyCode.A;
+        jump = KeyCode.Space;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RandomKey()
+    {
+        switch (Random.Range(0, 4))
+        {
+            case (0):
+
+                break;
+        }
+
     }
 }
