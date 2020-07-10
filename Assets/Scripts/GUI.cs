92a9e83ca36d51aa0f49535d2f7c8d791f9e1f3a
Selawen <37039183+Selawen@users.noreply.github.com>
2020-07-11 00:48:15 +0200

    changing keys works!
    - changes keys upon collision with enemy
    - added start fo gui

---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GUI : MonoBehaviour
+{
+    public TextMeshProUGUI keysText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateKeyText()
+    {
+        keysText.text = "not implemeted yet";
+    }
+}
