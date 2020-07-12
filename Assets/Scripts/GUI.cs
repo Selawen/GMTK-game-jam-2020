f8c8e849b43b31f7f498843dd1dff588a21d03ad
Ischa-w <ischa@westerveld.art>
2020-07-12 19:05:24 +0200

    Alles mooi gemaakt

--- Assets/Scripts/GUI.cs
+++ Assets/Scripts/GUI.cs
@@ -22,6 +22,9 @@ public class GUI : MonoBehaviour
 
     public void UpdateKeyText()
     {
+        changedKeys = GameObject.Find("InputManager").GetComponent<ChangeKey>();
+
+
         keysText.text = "Up: " + changedKeys.keycodeNames[(int)changedKeys.up] + "<br>" +
                         "Down: " + changedKeys.keycodeNames[(int)changedKeys.down] + "<br>" + 
                         "Left: " + changedKeys.keycodeNames[(int)changedKeys.left] + "<br>" + 
